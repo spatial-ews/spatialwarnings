62b4a096dbc4c0532b46af0085b15455640fa400
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2023-03-14 14:53:33 -0300

    Reorganize datasets + add dda

---
+++ R/datasets.R
@@ -0,0 +1,131 @@
+#' A list of binary matrices and their associated parameters 
+#'
+#' @rdname forestgap
+#' 
+#' @format A list of logical matrices which are the end results of simulations 
+#'   from Kubo's Forest Gap model along a gradient of increasing values of 
+#'   stress (see references). 
+#' 
+#' @source Generated using the implementation of Kubo's model in caspr 0.2.0 
+#'   \url{https://github.com/fdschneider/caspr}. 
+#' 
+#' @references
+#' 
+#' Kubo, T., Iwasa, Y., & Furumoto, N. (1996). Forest spatial dynamics with gap
+#'   expansion: Total gap area and gap size distribution. Journal of Theoretical
+#'   Biology, 180(3), 229-246. \doi{10.1006/jtbi.1996.0099}
+#' 
+"forestgap"
+
+#' @rdname forestgap
+#' 
+#' @format The parameters used for the simulations, as a data frame. 
+#' 
+#' @details Kubo's forest gap model has three parameters, \eqn{\alpha}{alpha} 
+#'   that controls the reproductive rate of trees, \eqn{d}{d} controls the 
+#'   non-spatialized mortality and \eqn{\delta}{delta} the increased mortality 
+#'   due to the presence of a neighboring gap. 
+#' 
+"forestgap.pars"
+
+
+
+#' @rdname arizona
+#' 
+#' @title Aerial views of vegetation from Arizona, USA
+#' 
+#' @format A list of logical matrices which were obtained through the 
+#'   classification of aerial images of vegetation taken in Arizona (USA). 
+#' 
+#' @source Derived from the images provided in the Supplementary Material of 
+#'   Rodriguez et al. (2017). 
+#' 
+#' @references
+#' 
+#' Rodriguez, F., A. G. Mayor, M. Rietkerk, and S. Bautista. 2017. A null model 
+#'   for assessing the cover-independent role of bare soil connectivity as 
+#'   indicator of dryland functioning and dynamics. Ecological Indicators.
+#' 
+"arizona"
+
+
+
+#' @rdname serengeti
+#' 
+#' @title Serengeti dataset
+#' 
+#' @description Vegetation data along a rainfall gradient in Serengeti national 
+#'   park. 
+#' 
+#' @details The data-set consists of a rectangular area of size 7.5 km x 90 km. 
+#'   These data are represented as a list of matrices. Each matrix is a moving 
+#'   window of 7.5 km x 7.5 km which moves my 2.5 km along the length of the 
+#'   rectangular data-set. 
+#'   Each entry in the matrix is vegetation data at a resolution of 30m as 
+#'   classified into binary units with 0 (grass) and 1 (forest).
+#'   The rainfall data provided here is the average rainfall (mm/yr) of a 
+#'   moving window of dimension 7.5km which moves my 2.5 km along the length 
+#'   of the rectangular data-set.   
+#' 
+#' @format A list of logical matrices 
+#' 
+#' @source Extracted from Eby's et al (2017) supplementary material 
+#'   \url{https://github.com/tee-lab/spacetime-csd/}
+#' 
+#' @references 
+#' 
+#' Eby, S., Agrawal, A., Majumder, S., Dobson, A.P. & Guttal, V. (2017). 
+#' Alternative stable states and spatial indicators of critical slowing down 
+#' along a spatial gradient in a savanna ecosystem: Global Ecology 
+#' and Biogeography, 26, 638-649
+#' 
+#' Reed, D. N., Anderson, T. M., Dempewolf, J., Metzger, K., & Serneels, S. (2009). 
+#' The spatial distribution of vegetation types in the Serengeti ecosystem: 
+#' the influence of rainfall and topographic relief on vegetation patch 
+#' characteristics. Journal of Biogeography, 36(4), 770-782.
+# 
+# The strings below have no effect except forcing R to read this file. 
+"serengeti" 
+
+#' @rdname serengeti
+#' 
+#' @format The annual rainfall corresponding to the matrices in the serengeti 
+#'   dataset, in the corresponding order. 
+#' 
+"serengeti.rain"
+
+
+
+# A list of binary matrices for the density-dependent aggregation model 
+#' @rdname dda
+#' 
+#' @title Density-dependent aggregation model
+#' 
+#' @description \code{dda} is a list of matrices representing results from the 
+#'   density-dependent aggregation model (Siteur et al. 2023) in \code{dda}.
+#'   \code{dda.pars} is a data frame with the model parameters. Each row of
+#'   \code{dda.pars} corresponds to a matrix in \code{dda}, in the same order. All
+#'   parameters were maintained constant, except for tau (see model
+#'   definition in Siteur et al. 2023).
+#' 
+#' @source Kindly provided by Koen Siteur
+#' 
+#' @examples 
+#' 
+#' ddasews <- lsw_sews(dda)
+#' plot(ddasews, along = dda.pars[ ,"tau"]) # tau is the changing parameter
+#' 
+#' display_matrix(dda[[6]]) 
+#' 
+#' @references 
+#' 
+#'   Siteur, Koen, Quan-Xing Liu, Vivi Rottschäfer, Tjisse van der Heide, Max Rietkerk,
+#'   Arjen Doelman, Christoffer Boström, and Johan van de Koppel. 2023. "Phase-Separation
+#'    Physics Underlies New Theory for the Resilience of Patchy Ecosystems." Proceedings 
+#'    of the National Academy of Sciences 120 (2): e2202683120. 
+#'    https://doi.org/10.1073/pnas.2202683120.
+"dda"
+
+#' @rdname dda
+#' 
+"dda.pars"
--- R/lsw_sews.R
+++ R/lsw_sews.R
@@ -1,7 +1,7 @@
+# 
 # The functions below fit the Lifshitz, Slyozov, Wagner (LSW) distribution to 
-# artificially generated patch sizes.
-# Patch size distributions are expected to approach the LSW distribution as cover goes 
-# to zero (Siteur et al., 2023).
+# artificially generated patch sizes. Patch size distributions are expected to 
+# approach the LSW distribution as cover goes to zero (Siteur et al., 2023).
 # 
 # Note that the patch sizes represent the patch radii, and not the patch area.
 # Patch radius can be estimated by assuming circular patches r=sqrt(a/2pi).
@@ -9,16 +9,67 @@
 # LSW does not fit well to distributions with a positive skewness, as patch radii 
 # greater than 1.5 times the mean patch size have zero probability.
 # 
-
+# 
+# 
+# 
+# 
+#'@title LSW-based indicators  
+#'
+#'@description LSW indicators for systems with density-dependent aggregation 
+#'
+#'
+#'@param mat A logical matrix (TRUE/FALSE values) or a list of these
+#' 
+#'@param wrap Determines whether patches are considered to wrap around the 
+#'  matrix when reaching on of its edges
+#' 
+#'@details 
+#' 
+#' # TODO
+#' 
+#' 
+#' @return 
+#' 
+#' \code{lsw_sews} returns an object of class \code{simple_sews_single}
+#'   (actually a list) if \code{mat} is a single matrix or an object of class 
+#'   \code{simple_sews_list} if \code{mat} is a list. You probably want to use some  
+#'   of the methods written for these complicated objects instead of extracting 
+#'   values directly (they are displayed using \code{print(<object>)}).
+#'
+#'@seealso dLSW, dda
+#'
+#'@references 
+#'  
+#'  # TODO Siteur 2023, other relevant papers ? 
+#'
+#'@examples 
+#'
+#' data(dda)
+#' data(dda.pars)
+#' 
+#' indics <- lsw_sews(dda)
+#' plot(indics, along = dda.pars[ ,"tau"]) 
+#' 
+#' # Compute individual indicators 
+#' 
+#' # Skewness of the distribution of patch radii
+#' radii_skewness <- compute_indicator(dda, raw_patch_radii_skewness)
+#' plot(radii_skewness, along = dda.pars[ ,"tau"])
+#' 
+#' # Aic weight of LSW distribution relative to a lognormal distribution 
+#' lsw_aicw <- compute_indicator(dda, raw_lsw_aicw)
+#' plot(lsw_aicw, along = dda.pars[ ,"tau"])
+#' 
+#' 
+#' 
 #'@export
 lsw_sews <- function(mat, wrap = FALSE) { 
-  compute_indicator(mat, raw_lsw_sews, 
+  compute_indicator(mat, internal_lsw_sews, 
                     wrap = wrap, 
                     taskname = "LSW-based indicators")
 }
 
-#'@export
-raw_lsw_sews <- function(mat, wrap) {
+internal_lsw_sews <- function(mat, wrap) {
   # Convert object to matrix form (or list of matrices) and check if it is
   # suitable for spatialwarnings
   mat <- convert_to_matrix(mat)
@@ -28,27 +79,54 @@ raw_lsw_sews <- function(mat, wrap) {
     check_mat(mat)
   }
   
-  # Get patch size distribution. Assume circle patches from the area. 
-  psd <- patchsizes(mat, wrap = wrap)
-  radii <- sqrt(psd / pi)
-  
   # Compute skewness and warn if positive
+  radii <- get_patch_radii(mat, wrap)
   skewness <- cpp_skewness(radii)
-  if ( skewness > 0 ) { 
-    warning("Positive skewness in radius distribution, LSW fit may fail")
-  }
-  
-  # Fit the LSW distribution 
-  fit_lsw <- fitLSW(radii)
-  
-  # Fit a lognormal
-  fit_lnorm <- fit_lnorm(radii)
+  aicw <- get_lsw_aicw(radii)
   
   # For now we just return the skewness and difference in AIC. We will need something 
   # a bit more fancy to have distributions fitting et al. 
   c(cover = mean(mat), 
     skewness = skewness, 
-    aic_diff = fit_lsw[["AIC"]] - fit_lnorm[["AIC"]])
+    lsw_aicw = aicw)
+}
+
+#'@rdname lsw_sews
+#'@export
+raw_patch_radii_skewness <- function(mat, wrap = FALSE) { 
+  radii <- get_patch_radii(mat, wrap)
+  c(psd_radii_skewnes = cpp_skewness(radii))
+}
+
+#'@rdname lsw_sews
+#'@export
+raw_lsw_aicw <- function(mat, wrap = FALSE) { 
+  # Get patch size distribution. Assume circle patches from the area. 
+  radii <- get_patch_radii(mat, wrap)
+  aicw <- get_lsw_aicw(radii)
+  return( c(lsw_aicw = aicw) )
+}
+
+get_patch_radii <- function(mat, wrap = FALSE) { 
+  # Get patch size distribution. Assume circle patches from the area. 
+  psd <- patchsizes(mat, wrap = wrap)
+  radii <- sqrt(psd / pi)
+}
+
+get_lsw_aicw <- function(radii) {
+  fit_lsw <- fitLSW(radii)
+  
+  # Fit a lognormal
+  fit_lnorm <- fit_lnorm(radii)
+  
+  # Compute AIC weight of LSW relative to lnorm 
+  minaic <- min(fit_lsw[["AIC"]], fit_lnorm[["AIC"]])
+  weight_lsw_min   <- exp( - 0.5 * ( fit_lsw[["AIC"]]   - minaic ) ) 
+  weight_lnorm_min <- exp( - 0.5 * ( fit_lnorm[["AIC"]] - minaic ) ) 
+  
+  aicw <- weight_lsw_min / ( weight_lsw_min + weight_lnorm_min )
+  
+  return(aicw)
 }
 
 fit_lnorm <- function(xs) { 
@@ -64,7 +142,7 @@ fit_lnorm <- function(xs) {
                fn = negll) 
   theta_est <- mle[["par"]]
   
-  aic <- 2 - ( - negll(theta_est) ) 
+  aic <- 2 - 2 * ( - negll(theta_est) ) 
   list(meanlog = theta_est[1], 
        sdlog   = theta_est[2], 
        AIC     = aic)
@@ -72,15 +150,64 @@ fit_lnorm <- function(xs) {
 
 # The Lifshitz, Slyozov, Wagner (LSW) probability density function. Used for plotting, 
 # and for computation of the AIC.
-dLSW <- function(r, mu){
-  ifelse(r<1.5*mu,
-         (4*r^2/(9*mu^3)) * (3*mu/(3*mu+r))^(7/3) * 
-          (3*mu/(3*mu-2*r))^(11/3) * exp(2*r/(2*r-3*mu)),
-         0)
+# 
+#'@title The Lifshitz-Slyozov-Wagner distribution 
+#'
+#'@description Density and distribution function for the Lifshitz-Slyozov-Wagner (LSW)
+#'  distribution with mean mu. 
+#'
+#'@param x vector of quantiles 
+#'
+#'@param vector of means 
+#' 
+#'@param log, log.p logical; if \code{TRUE}, probabilities p are given as log(p)
+#'
+#'@param lower.tail logical; if TRUE (default), probabilities are
+#'         \eqn{P[X \le x]} otherwise, \eqn{P[X > x]}
+#' 
+#'@details 
+#' # TODO
+#'
+#'@return
+#' dLSW gives the density, pLSW gives the distribution function, both as numerical 
+#'   vectors determined by the length of x. 
+#'
+#'@examples
+#' 
+#' # Plot the density 
+#' x <- seq(0, 10, l = 128) 
+#' plot(x, dLSW(x, mu = 3), type = "l", col = "black")
+#' lines(x, dLSW(x, mu = 5), type = "l", col = "red")
+#' lines(x, dLSW(x, mu = 7), type = "l", col = "blue")
+#' legend(x = 0, y = max(dLSW(x, mu = 3)), lty = 1, col = c("black", "red", "blue"), 
+#'        legend = paste("mu =", c(3, 5, 7)))
+#' 
+#'@export
+dLSW <- function(x, mu, log = FALSE){
+  p <- ifelse(x<1.5*mu,
+              (4*x^2/(9*mu^3)) * (3*mu/(3*mu+x))^(7/3) * 
+                (3*mu/(3*mu-2*x))^(11/3) * exp(2*x/(2*x-3*mu)),
+              0) 
+  if ( log ) { 
+    # TODO: actually implement this by working out the math on paper for 
+    # better numerical stability, but right now it works well
+    p <- log(p)
+  }
+  
+  p
+}
+
+# The LSW distribution, but standard 
+#'@rdname dLSW 
+#'
+#'@export 
+pLSW <- function(r, mu, lower.tail = TRUE, log.p = FALSE) { 
+  stop("TODO")
 }
 
 # The (approximate) cumulative LSW distribution. Used for plotting.
-cLSW <- function(r,mu){
+cLSW <- function(r,mu) {
+  # TODO: is there an analytical expression for the LSW distribution ? 
   x <- seq(0, 1.5, 0.01) * mu
   PDF <- dLSW(x, mu)
   CDF <- cumsum(PDF) / sum(PDF)
@@ -89,8 +216,8 @@ cLSW <- function(r,mu){
 
 # The (approximate) inverse cumulative LSW distribution. Used for drawing random 
 # patch radii
-icLSW <- function(X,mu){
-  x <- seq(0,1.5,0.001)*mu
+icLSW <- function(X, mu){
+  x <- seq(0,1.5,0.001) * mu
   PDF <- dLSW(x,mu)
   CDF <- cumsum(PDF)/sum(PDF)
   return(suppressWarnings((approx(CDF,x,X)$y)))  #I suppress warnings here. THe warning is because the approx function is fed Y values with the same X value. This is because the LSW distribution becomes very flat as it approaches 1.5.
@@ -105,41 +232,50 @@ dLdmu <- function(r, mu){
 # Function to fit the LSW probability density function. Returns the estimate of mu and 
 # the AIC for model comparison
 fitLSW <- function(r){
-#   mu_fit <- uniroot(function(mu){dLdmu(r,mu)},
-#                     interval=c(min(r),
-#                                max(r)))$root
-  mu_fit <- uniroot(function(mu){dLdmu(r,mu)},
-                    interval=c(0, max(r)))$root
-  AIC_fit <- 2-2*sum(log(dLSW(r,mu_fit)))
+  
+  negll <- function(mu) { 
+    nll <- dLSW(r, mu, log = TRUE) 
+    if ( any( ! is.finite(nll) ) ) { 
+      return(Inf)
+    }
+    - sum(nll)
+  }
+  
+  # Here we want to stick to values so that all r < 3mu/2 otherwise the log 
+  # likelihood is infinite, which means max(r) < 3mu/2, 
+  # i.e  mu > 2*max(r)/3. We find the smallest value that produces a finite negll and 
+  # use that as our lower bound for LL minimization later on. 
+  boundsearch <- uniroot(f = function(x) is.finite(negll(x)) - 0.5, 
+                         lower = min(r), 
+                         upper = max(r))
+  minbound <- boundsearch[["root"]] + boundsearch[["estim.prec"]]
+  
+  est <- optim(par = max(r), 
+               lower = minbound, 
+               upper = max(r), 
+               method = "L-BFGS-B", 
+               fn = negll)
+  
+  # Show dldmu 
+#   mus <- seq(0, max(r), l = 64)
+#   mus <- seq(2*max(r)/3 + epsilon, max(r), l = 64)
+#   plot(mus, sapply(mus, function(mu) { 
+#     dLdmu(r, mu)
+#     negll(mu)
+#   }))
+#   
+#   mu_fit <- uniroot(function(mu){ dLdmu(r, mu) },
+#                     interval = c(minbound, max(r)))$root
+#   AIC_fit <- 2 - 2 * sum(log(dLSW(r, mu_fit)))
+#   cat(sprintf("directll mu est: %s / uniroot mu est: %s / ll directll: %s\n", 
+#               mu_fit, est[["par"]], -est[["value"]]))
+  
+  mu_fit <- est[["par"]]
+  AIC_fit <- 2 - 2 * ( - est[["value"]] )
+  
   return(list(mu = mu_fit, 
               AIC = AIC_fit))
 }
 
 
-
-if ( FALSE ) { 
-  # generate 100 patch radii with a mu of 1
-  patch_radii <- icLSW(runif(100),mu = 1)
-
-  # fit the LSW distribution
-  ft <- fitLSW(patch_radii)
-  
-  # plot of histogram and PDF
-  hist(patch_radii,probability = T,xlab = "Patch radius (m)",main="Histogram of patch radii")
-  x <- seq(0,1.5,0.02)*ft$mu
-  lines(x,dLSW(x,ft$mu),col="red")
-  grid()
-  
-  # plot of cumulative probability distribution and CDF 
-  PXltx <- rank(patch_radii,ties.method = "average")/(length(patch_radii)+1)
-  plot(patch_radii,PXltx,pch=20,xlab = "Patch radius (m)",ylab="Cumulative probability",main="Cumulative probability distribution of patch radii")
-  lines(x,cLSW(x,mu = ft$mu),col="red")
-  grid()
-
-  # for some reason 1-CDF is often plotted and a log-log scale is used
-  PXltx <- rank(patch_radii,ties.method = "average")/(length(patch_radii)+1)
-  plot(patch_radii,1-PXltx,pch=20,log="xy",xlab = "Patch radius (m)",ylab="Cumulative probability",main="Cumulative probability distribution of patch radii")
-  lines(x,1-cLSW(x,mu = ft$mu),col="red")
-  grid()
-}
 
